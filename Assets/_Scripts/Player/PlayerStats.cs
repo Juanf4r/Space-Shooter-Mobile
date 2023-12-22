67708b06690ce4526dd9598d41c66abb4634ba93
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-12-22 13:35:31 -0700

    Add UI

--- Assets/_Scripts/Player/PlayerStats.cs
+++ Assets/_Scripts/Player/PlayerStats.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private float maxHealth;
     private float _health;
+
+    [SerializeField] private Image healthFill;
     void Start()
     {
         _health = maxHealth;
+        healthFill.fillAmount = _health/maxHealth;
     }
 
     public void PlayerTakeDamage(float damage)
     {
         _health -= damage;
-        if(_health < 0 )
+        healthFill.fillAmount = _health / maxHealth;
+        if (_health <= 0 )
         {
             Destroy(gameObject);
         }
